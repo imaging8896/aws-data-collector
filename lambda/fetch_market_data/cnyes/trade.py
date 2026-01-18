786892b66db9b8d2f1611764d4d973054520aac5
Ted Chen <imaging8896@gmail.com>
2026-01-18 16:36:15 +0800

    fix: Fix unable to get trades <=2021 year from cnyes

--- lambda/fetch_market_data/cnyes/trade.py
+++ lambda/fetch_market_data/cnyes/trade.py
@@ -44,7 +44,11 @@ class Index:
 def get_trades(indexes: list[Index], from_dt: datetime, to_dt: datetime):
     symbols = ",".join(str(index) for index in indexes)
     url = f"https://ws.api.cnyes.com/ws/api/v1/charting/histories?symbols={symbols}&from={int(to_dt.timestamp())}&to={int(from_dt.timestamp())}&resolution=D"
-
+    if from_dt.year <= 2021 or to_dt.year <= 2021:
+        if len(indexes) > 1:
+            raise Exception("CNYES API does not support multiple symbols for history endpoint")
+        request_index = indexes[0]
+        url = f"https://ws.api.cnyes.com/ws/api/v1/charting/history?resolution=D&symbol={symbols}&from={int(to_dt.timestamp())}&to={int(from_dt.timestamp())}&quote=1"
     response = requests.get(url, impersonate="chrome")
 
     if response.status_code != 200:
@@ -56,63 +60,75 @@ def get_trades(indexes: list[Index], from_dt: datetime, to_dt: datetime):
         raise Exception(f"API error: {data}")
     
     data = data['data']
-    if len(data) != len(indexes):
+    if isinstance(data, list) and len(data) != len(indexes):
         raise Exception(f"Returned data length {len(data)}\n{data}\ndoes not match requested indexes length {len(indexes)}")
 
     result = {}
-    for request_index, response_data in zip(indexes, data):
-        key = str(request_index)
-        if index_data := response_data.get(key):
-            if index_data.get('s') != 'ok':
-                raise Exception(f"Data for index {key} returned status\n{index_data}\nin\n{data}")
-            
-            data_dates = [date.fromtimestamp(time).isoformat() for time in index_data['t']]
-    
-            opening_prices = [Decimal(str(price)) for price in index_data['o']]
-            if len(opening_prices) != len(data_dates):
-                raise Exception(f"Data length mismatch for index {key}: dates({len(data_dates)}) vs openings({len(opening_prices)})\n{data}")
-    
-            closing_prices = [Decimal(str(price)) for price in index_data['c']]
-            if len(closing_prices) != len(data_dates):
-                raise Exception(f"Data length mismatch for index {key}: dates({len(data_dates)}) vs closings({len(closing_prices)})\n{data}")
-
-            high_prices = [Decimal(str(price)) for price in index_data['h']]
-            if len(high_prices) != len(data_dates):
-                raise Exception(f"Data length mismatch for index {key}: dates({len(data_dates)}) vs highs({len(high_prices)})\n{data}")
-
-            low_prices = [Decimal(str(price)) for price in index_data['l']]
-            if len(low_prices) != len(data_dates):
-                raise Exception(f"Data length mismatch for index {key}: dates({len(data_dates)}) vs lows({len(low_prices)})\n{data}")
-
-            volumes = [Decimal(str(price)) * Decimal(1000) for price in index_data['v']]
-            if len(volumes) != len(data_dates):
-                raise Exception(f"Data length mismatch for index {key}: dates({len(data_dates)}) vs volumes({len(volumes)})\n{data}")
-
-            turnovers = [Decimal(str(turnover)) for turnover in index_data['turnover']]
-            if len(turnovers) != len(data_dates):
-                raise Exception(f"Data length mismatch for index {key}: dates({len(data_dates)}) vs turnover({len(turnovers)})\n{data}")
-
-            # 'vwap' are also available but not used here
-            percent = Decimal(str(index_data['quote']['220064'])) if index_data['quote']['220064'] is not None else None
-            avg = Decimal(str(index_data['quote']['3404'])) if index_data['quote']['3404'] is not None else None
-            diff = Decimal(str(index_data['quote']['11'])) if index_data['quote']['11'] is not None else None
-            name = index_data['quote']['200009']
-
-            result[request_index] = {
-                "name": name,
-                "data": list(
-                    zip(
-                        data_dates,
-                        opening_prices,
-                        closing_prices,
-                        high_prices,
-                        low_prices,
-                        volumes,
-                        turnovers, # 成交金額
-                        strict=True,
-                    )
-                )
-            }
-        else:
-            raise Exception(f"Missing data for index {key} in response: {data}")
+    if isinstance(data, list):
+        for request_index, response_data in zip(indexes, data):
+            key = str(request_index)
+            if index_data := response_data.get(key):
+                result[request_index] = _get_data(index_data, data)
+            else:
+                raise Exception(f"Missing data for index {key} in response: {data}")
+    else:
+        result[indexes[0]] = _get_data(data, data)
+        
     return result
+
+def _get_data(index_data, all_data):
+    if index_data.get('s') != 'ok':
+        raise Exception(f"Data for index returned status\n{index_data}\nin\n{all_data}")
+    
+    data_dates = [date.fromtimestamp(time).isoformat() for time in index_data['t']]
+
+    opening_prices = [Decimal(str(price)) for price in index_data['o']]
+    if len(opening_prices) != len(data_dates):
+        raise Exception(f"Data length mismatch: dates({len(data_dates)}) vs openings({len(opening_prices)})\n{all_data}")
+
+    closing_prices = [Decimal(str(price)) for price in index_data['c']]
+    if len(closing_prices) != len(data_dates):
+        raise Exception(f"Data length mismatch: dates({len(data_dates)}) vs closings({len(closing_prices)})\n{all_data}")
+    high_prices = [Decimal(str(price)) for price in index_data['h']]
+    if len(high_prices) != len(data_dates):
+        raise Exception(f"Data length mismatch: dates({len(data_dates)}) vs highs({len(high_prices)})\n{all_data}")
+
+    low_prices = [Decimal(str(price)) for price in index_data['l']]
+    if len(low_prices) != len(data_dates):
+        raise Exception(f"Data length mismatch: dates({len(data_dates)}) vs lows({len(low_prices)})\n{all_data}")
+
+    if 'turnover' in index_data:
+        turnovers = [Decimal(str(turnover)) for turnover in index_data['turnover']]
+        volumes = [Decimal(str(price)) * Decimal(1000) for price in index_data['v']]
+    else:
+        turnovers = [Decimal(str(turnover)) for turnover in index_data['v']]
+        volumes = [None] * len(data_dates)
+
+    if len(volumes) != len(data_dates):
+        raise Exception(f"Data length mismatch: dates({len(data_dates)}) vs volumes({len(volumes)})\n{all_data}")
+
+    if len(turnovers) != len(data_dates):
+        raise Exception(f"Data length mismatch: dates({len(data_dates)}) vs turnover({len(turnovers)})\n{all_data}")
+
+    # 'vwap' are also available but not used here
+    percent = Decimal(str(index_data['quote']['220064'])) if index_data['quote']['220064'] is not None else None
+    avg = Decimal(str(index_data['quote']['3404'])) if index_data['quote']['3404'] is not None else None
+    diff = Decimal(str(index_data['quote']['11'])) if index_data['quote']['11'] is not None else None
+    name = index_data['quote']['200009']
+
+    return {
+        "name": name,
+        "data": list(
+            zip(
+                data_dates,
+                opening_prices,
+                closing_prices,
+                high_prices,
+                low_prices,
+                volumes,
+                turnovers, # 成交金額
+                strict=True,
+            )
+        )
+    }
+    
